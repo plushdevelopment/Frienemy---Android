d8c186570501432c22b4795e91febde2996a50f8
Ross Chapman <ross.chapman@me.com>
2011-11-02 10:54:05 -0400

    first commit

---
+++ src/com/frienemy/activities/FrienemyActivity.java
@@ -0,0 +1,13 @@
+package com.frienemy.activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class FrienemyActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
---
+++ src/com/frienemy/services/FrienemyService.java
@@ -0,0 +1,19 @@
+package com.frienemy.services;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+public class FrienemyService extends Service {
+
+	public FrienemyService() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public IBinder onBind(Intent arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
