555ffe188bd12d9be204d18457dacec42f43bab5
Ross Chapman <ross.chapman@me.com>
2011-11-18 11:24:25 -0500

    Added Facebook permissions and storing of the access_token

--- src/com/frienemy/activities/FrienemyActivity.java
+++ src/com/frienemy/activities/FrienemyActivity.java
@@ -3,27 +3,53 @@ package com.frienemy.activities;
 import android.app.Activity;
 import android.os.Bundle;
 import android.content.Intent;
+import android.content.SharedPreferences;
+
 import com.facebook.android.*;
 import com.facebook.android.Facebook.*;
 
 public class FrienemyActivity extends Activity {
     
 	Facebook facebook = new Facebook("124132700987915");
+	String FILENAME = "AndroidSSO_data";
+    private SharedPreferences mPrefs;
 	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        /*
+         * Get existing access_token if any
+         */
+        mPrefs = getPreferences(MODE_PRIVATE);
+        String access_token = mPrefs.getString("access_token", null);
+        long expires = mPrefs.getLong("access_expires", 0);
+        if(access_token != null) {
+            facebook.setAccessToken(access_token);
+        }
+        if(expires != 0) {
+            facebook.setAccessExpires(expires);
+        }
         
-        facebook.authorize(this, new DialogListener() {
-            public void onComplete(Bundle values) {}
+        /*
+         * Only call authorize if the access_token has expired.
+         */
+        if(!facebook.isSessionValid()) {
+        	facebook.authorize(this, new String[] {"read_stream", "offline_access", "friends_relationships", "friends_relationship_details", "user_relationships", "user_relationship_details", "friends_likes", "user_likes", "publish_stream"}, new DialogListener() {
+        		public void onComplete(Bundle values) {
+        			SharedPreferences.Editor editor = mPrefs.edit();
+                    editor.putString("access_token", facebook.getAccessToken());
+                    editor.putLong("access_expires", facebook.getAccessExpires());
+                    editor.commit();
+        		}
 
-            public void onFacebookError(FacebookError error) {}
+        		public void onFacebookError(FacebookError error) {}
 
-            public void onError(DialogError e) {}
+        		public void onError(DialogError e) {}
 
-            public void onCancel() {}
-        });
+        		public void onCancel() {}
+        	});
+        }
         
     }
     
