b1ea956affba3ba9456f53c50f132d4b87364db5
Ross Chapman <ross.chapman@me.com>
2011-11-18 11:10:28 -0500

    Added Facebook SDK to project in a not so graceful manner

--- src/com/frienemy/activities/FrienemyActivity.java
+++ src/com/frienemy/activities/FrienemyActivity.java
@@ -2,12 +2,34 @@ package com.frienemy.activities;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.content.Intent;
+import com.facebook.android.*;
+import com.facebook.android.Facebook.*;
 
 public class FrienemyActivity extends Activity {
-    /** Called when the activity is first created. */
+    
+	Facebook facebook = new Facebook("124132700987915");
+	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        facebook.authorize(this, new DialogListener() {
+            public void onComplete(Bundle values) {}
+
+            public void onFacebookError(FacebookError error) {}
+
+            public void onError(DialogError e) {}
+
+            public void onCancel() {}
+        });
+        
+    }
+    
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        facebook.authorizeCallback(requestCode, resultCode, data);
     }
 }
