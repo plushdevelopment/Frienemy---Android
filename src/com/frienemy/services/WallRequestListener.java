3d1812ab3140f12873ef3337fc32532999b7ca15
Ross Chapman <ross.chapman@me.com>
2011-12-10 20:31:19 -0500

    Try/Catches galore

--- src/com/frienemy/services/WallRequestListener.java
+++ src/com/frienemy/services/WallRequestListener.java
@@ -22,11 +22,11 @@ public class WallRequestListener implements RequestListener {
 
 	private static final String TAG = WallRequestListener.class.getSimpleName();
 	private Context context;
-	
+
 	public WallRequestListener(Context context) {
 		this.context = context;
 	}
-	
+
 	public void onComplete(String response, Object state) {
 		try {
 			final JSONObject json = new JSONObject(response);
@@ -35,7 +35,7 @@ public class WallRequestListener implements RequestListener {
 			for (int i=0; i<l; i++) {
 				JSONObject o = d.getJSONObject(i);
 				String uid = o.getString("id");
-				Post post = Post.querySingle(context, Post.class, null, String.format("uid = %s", uid));
+				Post post = Post.querySingle(context, Post.class, null, String.format("uid = \"%s\"", uid));
 				if (post == null) {
 					post = new Post(context);
 					post.uid = uid;
@@ -47,51 +47,157 @@ public class WallRequestListener implements RequestListener {
 						existingComment.delete();
 					}
 				}
-				post.attribution = o.getString("attribution");
-				post.caption = o.getString("caption");
-				post.createdTime = o.getString("created_time");
-				post.description = o.getString("description");
-				post.icon = o.getString("icon");
-				post.link = o.getString("link");
-				post.message = o.getString("message");
-				post.name = o.getString("name");
-				post.objectId = o.getString("object_id");
-				post.picture = o.getString("picture");
-				post.source = o.getString("source");
-				post.story = o.getString("story");
-				post.type = o.getString("type");
-				post.updatedTime = o.getString("updated_time");
-				JSONObject fromObject = o.getJSONObject("from");
-				Friend fromFriend = Friend.friendInContextForJSONObject(context, fromObject);
-				post.fromFriend = fromFriend;
-				JSONObject toObject = o.getJSONObject("to");
-				Friend toFriend = Friend.friendInContextForJSONObject(context, toObject);
-				post.toFriend = toFriend;
-				JSONObject likesObject = o.getJSONObject("likes");
-				post.likesCount = likesObject.getInt("count");
-				JSONArray likesArray = likesObject.getJSONArray("data");
-				for (int x=0; x<likesArray.length(); x++) {
-					Like like = new Like(context);
-					Friend likeFriend = Friend.friendInContextForKeyWithStringValue(context, "uid", likesArray.getJSONObject(x).getString("uid")); 
-					like.friend = likeFriend;
-					like.post = post;
+				try {
+					post.attribution = o.getString("attribution");
+				} catch (JSONException e) {
+					e.printStackTrace();
 				}
-				
-				JSONObject commentsObject = o.getJSONObject("comments");
-				post.commentsCount = commentsObject.getInt("count");
-				JSONArray commentsArray = commentsObject.getJSONArray("data");
-				for (int x=0; x<commentsArray.length(); x++) {
-					Comment comment = new Comment(context);
-					JSONObject commentFromFriend = commentsArray.getJSONObject(x).getJSONObject("from");
-					Friend commentFriend = Friend.friendInContextForKeyWithStringValue(context, "uid", commentFromFriend.getString("uid"));
-					comment.fromFriend = commentFriend;
-					comment.toFriend = post.fromFriend;
-					comment.post = post;
-					comment.message = commentsArray.getJSONObject(x).getString("message");
-					comment.createdTime = commentsArray.getJSONObject(x).getString("created_time");
-					comment.uid = commentsArray.getJSONObject(x).getString("id");
+				try {
+					post.caption = o.getString("caption");
+				} catch (JSONException e) {
+					e.printStackTrace();
 				}
-				
+				try {
+					post.createdTime = o.getString("created_time");
+				} catch (JSONException e) {
+					e.printStackTrace();
+				}
+				try {
+					post.description = o.getString("description");
+				} catch (JSONException e) {
+					e.printStackTrace();
+				}
+				try {
+					post.icon = o.getString("icon");
+				} catch (JSONException e) {
+					e.printStackTrace();
+				}
+				try {
+					post.link = o.getString("link");
+				} catch (JSONException e) {
+					e.printStackTrace();
+				}
+				try {
+					post.message = o.getString("message");
+				} catch (JSONException e) {
+					e.printStackTrace();
+				}
+				try {
+					post.name = o.getString("name");
+				} catch (JSONException e) {
+					e.printStackTrace();
+				}
+				try {
+					post.objectId = o.getString("object_id");
+				} catch (JSONException e) {
+					e.printStackTrace();
+				}
+				try {
+					post.picture = o.getString("picture");
+				} catch (JSONException e) {
+					e.printStackTrace();
+				}
+				try {
+					post.source = o.getString("source");
+				} catch (JSONException e) {
+					e.printStackTrace();
+				}
+				try {
+					post.story = o.getString("story");
+				} catch (JSONException e) {
+					e.printStackTrace();
+				}
+				try {
+					post.type = o.getString("type");
+				} catch (JSONException e) {
+					e.printStackTrace();
+				}
+				try {
+					post.updatedTime = o.getString("updated_time");
+				} catch (JSONException e) {
+					e.printStackTrace();
+				}
+				try {
+					JSONObject fromObject = o.getJSONObject("from");
+					Friend fromFriend = Friend.friendInContextForJSONObject(context, fromObject);
+					post.fromFriend = fromFriend;
+				} catch (JSONException e) {
+					e.printStackTrace();
+				}
+				try {
+					JSONObject toObject = o.getJSONObject("to");
+					Friend toFriend = Friend.friendInContextForJSONObject(context, toObject);
+					post.toFriend = toFriend;
+				} catch (JSONException e) {
+					e.printStackTrace();
+				}
+				try {
+					JSONObject likesObject = o.getJSONObject("likes");
+					try {
+						post.likesCount = likesObject.getInt("count");
+					} catch (JSONException e) {
+						e.printStackTrace();
+					}
+					try {
+						JSONArray likesArray = likesObject.getJSONArray("data");
+						for (int x=0; x<likesArray.length(); x++) {
+							Like like = new Like(context);
+							Friend likeFriend = Friend.friendInContextForKeyWithStringValue(context, "uid", likesArray.getJSONObject(x).getString("uid")); 
+							like.friend = likeFriend;
+							like.post = post;
+						}
+					} catch (JSONException e) {
+						e.printStackTrace();
+					}
+					try {
+						JSONObject commentsObject = o.getJSONObject("comments");
+						try {
+							post.commentsCount = commentsObject.getInt("count");
+						} catch (JSONException e) {
+							e.printStackTrace();
+						}
+						try {
+							JSONArray commentsArray = commentsObject.getJSONArray("data");
+							for (int x=0; x<commentsArray.length(); x++) {
+								Comment comment = new Comment(context);
+								try {
+									JSONObject commentFromFriend = commentsArray.getJSONObject(x).getJSONObject("from");
+									try {
+										Friend commentFriend = Friend.friendInContextForKeyWithStringValue(context, "uid", commentFromFriend.getString("uid"));
+										comment.fromFriend = commentFriend;
+										comment.toFriend = post.fromFriend;
+										comment.post = post;
+										try {
+											comment.message = commentsArray.getJSONObject(x).getString("message");
+										} catch (JSONException e) {
+											e.printStackTrace();
+										}
+										try {
+											comment.createdTime = commentsArray.getJSONObject(x).getString("created_time");
+										} catch (JSONException e) {
+											e.printStackTrace();
+										}
+										try {
+											comment.uid = commentsArray.getJSONObject(x).getString("id");
+										} catch (JSONException e) {
+											e.printStackTrace();
+										}
+									} catch (JSONException e) {
+										e.printStackTrace();
+									}
+								} catch (JSONException e) {
+									e.printStackTrace();
+								}
+							}
+						} catch (JSONException e) {
+							e.printStackTrace();
+						}
+					} catch (JSONException e) {
+						e.printStackTrace();
+					}
+				} catch (JSONException e) {
+					e.printStackTrace();
+				}
 				post.save();
 				Log.d(TAG, o.toString());
 			}
@@ -103,22 +209,22 @@ public class WallRequestListener implements RequestListener {
 
 	public void onIOException(IOException e, Object state) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	public void onFileNotFoundException(FileNotFoundException e, Object state) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	public void onMalformedURLException(MalformedURLException e, Object state) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	public void onFacebookError(FacebookError e, Object state) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 }
