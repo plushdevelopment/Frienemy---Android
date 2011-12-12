bd75aa2d9d7fe13a5650fbbaf8d52a7d12289c6a
Ross Chapman <ross.chapman@me.com>
2011-12-12 15:47:30 -0500

    prevented stalkers list from being displayed under request finishes

--- src/com/frienemy/activities/StalkerActivity.java
+++ src/com/frienemy/activities/StalkerActivity.java
@@ -33,10 +33,8 @@ public class StalkerActivity extends ListActivity implements OnClickListener, Wa
 	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.stalkers);
-		
 		 TextView v = (TextView) findViewById(R.id.title);
 	        v.setText("Stalkers");
-		updateView();
 		setUpListeners();
 		getFacebookWall();
 	}
